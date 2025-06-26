82120c8d689ee967e5d6cb62ff5fc5c5deff467e
Jyoti Gupta <160124343+Jo-shreer@users.noreply.github.com>
2025-06-26 11:06:13 +0100

    Create ClassAndObject.java

---
+++ Java basics/ClassAndObject.java
@@ -0,0 +1,10 @@
+public class Car 
+{
+    String brand = "Toyota"
+
+    public static void main(String[] args) 
+   {
+        Car myCar = new Car();
+        System.out.println("Brand: " + myCar.brand);
+    }
+}
