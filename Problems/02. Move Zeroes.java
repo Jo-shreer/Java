3854c3de8444f0c16eab9f631848239d1829b53b
Jyoti Gupta <160124343+Jo-shreer@users.noreply.github.com>
2025-08-23 23:00:13 +0100

    Update 02. Move Zeroes.java

--- Problems/02. Move Zeroes.java
+++ Problems/02. Move Zeroes.java
@@ -7,3 +7,37 @@ Input:  [0,1,0,3,12]
 Output: [1,3,12,0,0]
 
 */
+import java.util.*;
+
+public class Main
+{
+    public static void moveZeros(int arr[])
+    {
+        int size = arr.length;
+        int index = 0;
+        for(int i =0; i < size; i++)
+        {
+            if(arr[i] != 0)
+            {
+                arr[index] = arr[i];
+                index++;
+            }
+        }
+        
+        while(index < size)
+        {
+            arr[index] = 0;
+            index++;
+        }
+        
+    }
+    public static void main(String args[])
+    {
+        int arr[] = {0,1,0,3,12};
+        moveZeros(arr);
+        System.out.println("res1:" + Arrays.toString(arr));
+    }
+}
+
+op
+res1:[1, 3, 12, 0, 0]
