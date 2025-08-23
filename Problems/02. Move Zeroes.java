6e9cb5d098d121ce6bc78496554e3a109553dcb2
Jyoti Gupta <160124343+Jo-shreer@users.noreply.github.com>
2025-08-23 22:48:01 +0100

    Create 02. Move Zeroes.java

---
+++ Problems/02. Move Zeroes.java
@@ -0,0 +1,9 @@
+/*
+Problem Statement
+Given an integer array nums, move all 0s to the end of it while maintaining the relative order of the non-zero elements.
+⚠️ Do this in-place without making a copy of the array.
+
+Input:  [0,1,0,3,12]
+Output: [1,3,12,0,0]
+
+*/
